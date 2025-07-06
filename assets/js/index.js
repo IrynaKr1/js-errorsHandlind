04f19c59d33b8b527510b69ddda584c8cdbf69b4
IrynaKr1 <krutsiryna@gmail.com>
2025-07-06 14:55:17 +0200

    Add answer to second task

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,15 +21,53 @@ function pow(base, exponent) {
 }
 
 try {
-    console.log(pow("2", 3));  
+  console.log(pow('2', 3));
 } catch (error) {
-    console.error("Error:", error.message);
+  console.error('Error:', error.message);
 }
 
 try {
-    console.log(pow(2, 3.5));  
+  console.log(pow(2, 3.5));
 } catch (error) {
-    console.log(error);
+  console.log(error);
 }
 
-console.log(pow(2, 3)); 
+console.log(pow(2, 3));
+
+// 2. Email validation
+
+function emailValidation(email) {
+  if (typeof email != 'string') {
+    throw new TypeError('Email should be a string');
+  }
+
+  if (email.indexOf('@') === -1) {
+    throw new Error('Email should contain @ symbol');
+  }
+
+  if (email.startsWith('@') || email.endsWith('@')) {
+    throw new Error('Email cannot start or end with @ symbol');
+  }
+
+  return email;
+}
+
+console.log(emailValidation('ira@test.test'));
+
+try {
+  console.log(emailValidation('iratest.test'));
+} catch (error) {
+  console.error('Error:', error.message);
+}
+
+try {
+  console.log(emailValidation('@ira@test.test'));
+} catch (error) {
+  console.error('Error:', error.message);
+}
+
+try {
+  console.log(emailValidation('ira@test.test@'));
+} catch (error) {
+  console.error('Error:', error.message);
+}
